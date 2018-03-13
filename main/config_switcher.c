3acc49ac4aff14cb44cc366be27a7ad80bc160d1
Benjamin Aigner <aignerb@technikum-wien.at>
2018-03-13 15:46:36 +0100

    Changed default behaviour if slot cannot be loaded

--- main/config_switcher.c
+++ main/config_switcher.c
@@ -247,13 +247,17 @@ void configSwitcherTask(void * params)
         ret = halStorageLoad(DEFAULT,&currentConfig,tid);
         //for default, we try here again (in the case we had to build a new
         //fresh default config
-        if(ret != ESP_OK) ret = halStorageLoad(DEFAULT,&currentConfig,tid);
+        if(ret != ESP_OK) 
+        {
+          halStorageCreateDefault(tid);
+          ret = halStorageLoad(DEFAULT,&currentConfig,tid);
+        }
         ESP_LOGD(LOG_TAG,"loading default");
       } else if(strcmp(command,"__RESTOREFACTORY") == 0) {
         ret = halStorageDeleteSlot(0,tid);
         
         halStorageCreateDefault(tid);
-        halStorageFinishTransaction(tid);
+        ret = halStorageLoad(DEFAULT,&currentConfig,tid);
         if(ret != ESP_OK)
         {
           ESP_LOGE(LOG_TAG,"Error deleting all slots");

2a4fcfc8e219712d9fb218cca7d7a727e7245055
Benjamin Klaus <klaus@technikum-wien.at>
2018-03-21 12:06:03 +0100

    adapted are communicator, using ws utils, using constants

--- webguitest/spiffs_content/js/arecomm.js
+++ webguitest/spiffs_content/js/arecomm.js
@@ -1,10 +1,6 @@
-function ARECommunicator() {
-    //The base URI that ARE runs at
-    //var _baseURI = "http://" + window.location.host + "/rest/";
-    var VALUE_CONSTANT = 'VALUES:';
-    var _baseURI = 'http://' + window.location.hostname + ':8091/rest/';
-    var _websocketUrl = 'ws://' + window.location.hostname + ':8092/ws/astericsData';
-    var _websocket = null;
+function ARECommunicator(socket) {
+    var _restURI = 'http://' + window.location.hostname + ':8091/rest/';
+    var _websocket = socket;
     var _valueHandler = null;
     var _component = 'LipMouse.1';
     var _inputPort = 'send';
@@ -26,58 +22,17 @@ function ARECommunicator() {
         if (!value) return;
 
         //use GET sendDataToInputPort() for legacy reasons (phones that do only support GET requests)
-        var url = _baseURI + "runtime/model/components/" + encodeParam(_component) + "/ports/" + encodeParam(_inputPort) + "/data/" + encodeParam(value);
+        var url = _restURI + "runtime/model/components/" + encodeParam(_component) + "/ports/" + encodeParam(_inputPort) + "/data/" + encodeParam(value);
         var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", url); // false for synchronous request
+        xmlHttp.open("GET", url);
         xmlHttp.send(value);
         return new Promise((resolve, reject) => {
-            initWebsocket(_websocketUrl).then(function () {
-                var result = '';
-                var timeoutHandler = setTimeout(function () {
-                    //console.log("timeout von command: " + value);
-                    resolve(result);
-                }, 3000);
-                _websocket.onmessage = function (evt) {
-                    if (evt.data && evt.data.indexOf(VALUE_CONSTANT) > -1) {
-                        if (L.isFunction(_valueHandler)) {
-                            _valueHandler(evt.data);
-                        }
-                        return;
-                    }
-                    clearTimeout(timeoutHandler);
-                    result += evt.data + '\n';
-                    timeoutHandler = setTimeout(function () {
-                        console.log("got result: " + result);
-                        resolve(result)
-                    }, 200);
-                };
+            ws.initWebsocket(C.ARE_WEBSOCKET_URL, _websocket).then(function (socket) {
+                _websocket = socket;
+                ws.handleData(_websocket, _valueHandler).then(resolve);
             }, function () {
                 reject();
             });
         });
     };
-
-    function initWebsocket(url) {
-        return new Promise((resolve, reject) => {
-            if (!_websocket || _websocket.readyState != _websocket.OPEN) {
-                if (_websocket) {
-                    _websocket.close();
-                }
-                _websocket = new WebSocket(url);
-                _websocket.onopen = function (evt) {
-                    console.info("websocket opened!");
-                    resolve();
-                };
-                _websocket.onclose = function (evt) {
-                    console.info("websocket closed!");
-                };
-                _websocket.onerror = function (evt) {
-                    console.info("websocket error!");
-                    reject();
-                };
-            } else {
-                resolve();
-            }
-        });
-    }
 }

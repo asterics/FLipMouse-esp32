1ef3311b357cfbba1185da05ada9c86772b3226f
Benjamin Klaus <klaus@technikum-wien.at>
2018-04-27 08:17:30 +0200

    fixed broken live pressure values from mock communicator

--- webguitest/spiffs_content/js/mockcomm.js
+++ webguitest/spiffs_content/js/mockcomm.js
@@ -13,7 +13,7 @@ function MockCommunicator() {
         thiz.pressure = thiz.pressure || 500;
         thiz.x = thiz.x || 0;
         thiz.y = thiz.y || 0;
-        thiz.incrementP = thiz.increment || 1;
+        thiz.incrementP = thiz.incrementP || 1;
         thiz.incrementXY = thiz.increment || 1;
 
         return new Promise(function (resolve) {
@@ -22,11 +22,11 @@ function MockCommunicator() {
             } else if (value.indexOf('AT SR') > -1) {
                 _invervalHandler = setInterval(function () {
                     if (L.isFunction(_valueHandler)) {
-                        thiz.pressure += thiz.increment;
+                        thiz.pressure += thiz.incrementP;
                         thiz.x += thiz.incrementXY*getRandomInt(-5,5);
                         thiz.y += thiz.incrementXY*(-1)*getRandomInt(-5,5);
                         if(thiz.pressure > 550 || thiz.pressure < 450) {
-                            thiz.increment *= -1;
+                            thiz.incrementP *= -1;
                         }
                         if(thiz.y > 100 || thiz.y < -100 || thiz.x > 100 || thiz.x < -100) {
                             thiz.incrementXY *= -1;

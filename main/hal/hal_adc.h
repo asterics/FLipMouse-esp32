74913c43becd526633ce8f7edffb590fd45a553f
Benjamin Aigner <aignerb@technikum-wien.at>
2018-07-03 09:52:16 +0200

    Pin change

--- main/hal/hal_adc.h
+++ main/hal/hal_adc.h
@@ -60,19 +60,19 @@
 /** @brief ADC input pin for "up" channel of FSR
  * @note For adapting this pin, change HAL_IO_ADC_CHANNEL_UP as well!
  * @see HAL_IO_ADC_CHANNEL_UP */
-#define HAL_IO_PIN_ADC_UP       36
+#define HAL_IO_PIN_ADC_UP       39
 /** @brief ADC input pin for "down" channel of FSR
  * @note For adapting this pin, change HAL_IO_ADC_CHANNEL_DOWN as well!
  * @see HAL_IO_ADC_CHANNEL_DOWN */
-#define HAL_IO_PIN_ADC_DOWN     32
+#define HAL_IO_PIN_ADC_DOWN     35
 /** @brief ADC input pin for "left" channel of FSR
  * @note For adapting this pin, change HAL_IO_ADC_CHANNEL_LEFT as well!
  * @see HAL_IO_ADC_CHANNEL_LEFT */
-#define HAL_IO_PIN_ADC_LEFT     35
+#define HAL_IO_PIN_ADC_LEFT     32
 /** @brief ADC input pin for "right" channel of FSR
  * @note For adapting this pin, change HAL_IO_ADC_CHANNEL_RIGHT as well!
  * @see HAL_IO_ADC_CHANNEL_RIGHT */
-#define HAL_IO_PIN_ADC_RIGHT    39
+#define HAL_IO_PIN_ADC_RIGHT    36
 /** @brief ADC input pin for pressure sensor MPX
  * @note For adapting this pin, change HAL_IO_ADC_CHANNEL_PRESSURE as well!
  * @see HAL_IO_ADC_CHANNEL_PRESSURE */
@@ -81,19 +81,19 @@
 /** @brief ADC channel for FSR up
  * @note For adapting this channel, change HAL_IO_PIN_ADC_UP as well!
  * @see HAL_IO_PIN_ADC_UP */
-#define HAL_IO_ADC_CHANNEL_UP       ADC1_CHANNEL_0
+#define HAL_IO_ADC_CHANNEL_UP       ADC1_CHANNEL_3
 /** @brief ADC channel for FSR down
  * @note For adapting this channel, change HAL_IO_PIN_ADC_DOWN as well!
  * @see HAL_IO_PIN_ADC_DOWN */
-#define HAL_IO_ADC_CHANNEL_DOWN     ADC1_CHANNEL_4
+#define HAL_IO_ADC_CHANNEL_DOWN     ADC1_CHANNEL_7
 /** @brief ADC channel for FSR left
  * @note For adapting this channel, change HAL_IO_PIN_ADC_LEFT as well!
  * @see HAL_IO_PIN_ADC_LEFT */
-#define HAL_IO_ADC_CHANNEL_LEFT     ADC1_CHANNEL_7
+#define HAL_IO_ADC_CHANNEL_LEFT     ADC1_CHANNEL_4
 /** @brief ADC channel for FSR right
  * @note For adapting this channel, change HAL_IO_PIN_ADC_RIGHT as well!
  * @see HAL_IO_PIN_ADC_RIGHT */
-#define HAL_IO_ADC_CHANNEL_RIGHT    ADC1_CHANNEL_3
+#define HAL_IO_ADC_CHANNEL_RIGHT    ADC1_CHANNEL_0
 /** @brief ADC channel for pressure sensor
  * @note For adapting this channel, change HAL_IO_PIN_ADC_PRESSURE as well!
  * @see HAL_IO_PIN_ADC_PRESSURE */

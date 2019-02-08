7eaac1e85b8acd5daa225c64e8f25e28aeacec8e
Benjamin Aigner <aignerb@technikum-wien.at>
2019-02-08 10:44:44 +0100

    Removed BLE stuff from Neil Kolban, added the Espressif example

--- main/ble_hid/component.mk
+++ main/ble_hid/component.mk
@@ -1,12 +1,7 @@
 #
-# Main component makefile.
+# Main Makefile. This is basically the same as a component makefile.
 #
-# This Makefile can be left empty. By default, it will take the sources in the 
-# src/ directory, compile them and link them into lib(subdirectory_name).a 
-# in the build directory. This behaviour is entirely configurable,
-# please read the ESP-IDF documents if you need to do this.
-COMPONENT_ADD_INCLUDEDIRS=.
 
-## Uncomment the following line to enable exception handling 
-#CXXFLAGS+=-fexceptions
-#CXXFLAGS+= -std=c++11
+ifeq ($(GCC_NOT_5_2_0), 1)
+hid_device_le_prf.o: CFLAGS += -Wno-unused-const-variable 
+endif

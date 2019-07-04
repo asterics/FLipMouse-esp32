5f643c75abf856b39903bd6be78f6692d6906a8b
Benjamin Aigner <aignerb@technikum-wien.at>
2019-07-04 18:14:50 +0200

    Changed default deadzone values

--- webguitest/makespiffs.sh
+++ webguitest/makespiffs.sh
@@ -1,2 +1,7 @@
-mkspiffs -c spiffs_content -b 4096 -p 256 -s 2031612 spiffs_image.img
+
+## this line is used with the OTA partitions file (different offset)
+#mkspiffs -c spiffs_content -b 4096 -p 256 -s 0x170000 spiffs_image.img
+#esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 write_flash --flash_size detect 0x290000 spiffs_image.img
+## this line is used with the debugging partitions file
+mkspiffs -c spiffs_content -b 4096 -p 256 -s 0x1F0000 spiffs_image.img
 esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 write_flash --flash_size detect 0x210000 spiffs_image.img

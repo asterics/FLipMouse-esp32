8b5227e50fb9212c0fb2d64e7b37bc30fbd1ffcc
Benjamin Aigner <aignerb@technikum-wien.at>
2019-07-31 11:06:56 +0200

    Added script for auto-generating a new webGUI image (SPIFFS)

--- webgui/README.md
+++ webgui/README.md
@@ -1,6 +1,43 @@
-# FlipMouse GUI
-This is an early test version of the FlipMouse WebGUI for the ESP32.
-It will be merged with the main FlipMouse ESP32 firmware when it is in a usable stage.
+# FlipMouse/FABI Web GUI
+
+This is a JS/HTML WebGUI for FABI and FLipMouse.
+It serves as configuration GUI for setting up a FABI or FLipMouse device.
+
+The WiFi hotspot is enabled by pressing the internal button for 5 seconds.
+Any WiFi capable device can connect to the hotspot, the ESP32 will act as captive portal.
+
+If the GUI is not loaded automatically, please open this URL in the browser:
+__http://192.168.4.1/__
+
+# Install
+
+## Prerequisites
+
+* mkspiffs tool: https://github.com/igrr/mkspiffs
+* gzip
+* esp-idf (for uploading via esptool.py)
+* npm (nodejs)
+
+## Compiling & Uploading
+
+`bash ./makespiffs.sh <FABI/FM> <USB port>`
+
+Example for a FLipMouse on port /dev/ttyUSB0:
+
+`bash ./makespiffs.sh FM /dev/ttyUSB0`
+
+# Folders & Files
+
+* __src__ original WebGUI source files
+* __minified__ output of npm for minified source files
+* __minified_gz__ output of gzip for compressed&minified source files
+* __spiffs_content__ additional files which are packed into the image file (factory configuration)
+* __makespiffs.sh__ Bash script for generating & flashing the SPIFFS image to the ESP32
+* __spiffs_FABI.img__ FABI WebGUI image with additional files
+* __spiffs_FM.img__ FLipMouse WebGUI image with additional files
+
+
+# Acknowledgements
 
 The code is based on the ESP32 IDF examples,
 as well as the ESP32light project by Luca Dentella, see:
--- webgui/makespiffs.sh
+++ webgui/makespiffs.sh
@@ -1,7 +1,43 @@
+#set paths according to FABI/FM
+if [ "$1" = "FM" ]; then
+    IMAGEFILE="spiffs_FM.img"
+    SRCPATH="./minified_gz/flipmouse"
+    SPIFFSFILES="./spiffs_content/flipmouse/*"
+    echo "Compiling for FLipMouse"
+elif [ "$1" = "FABI" ]; then
+    IMAGEFILE="spiffs_FABI.img"
+    SRCPATH="./minified_gz/fabi"
+    SPIFFSFILES="./spiffs_content/fabi/*"
+    echo "Compiling for FABI"
+else
+    echo "Select either FM or FABI!"
+    exit
+fi
+
+if [ "$2" = "" ]; then
+    echo "Please select an USB port!"
+    exit
+fi
+
+#minify sources
+cd ..
+npm install
+cd webgui
+
+#remove old gz files & create new ones
+rm -r ./minified_gz
+cp -r ./minified ./minified_gz
+cd minified_gz
+find . -type f ! -name '*.gz' -exec gzip "{}" \;
+cd ..
+
+#update additional SPIFFS files (config/settings)
+cp -r $SPIFFSFILES $SRCPATH
+
 
 ## this line is used with the OTA partitions file (different offset)
-#mkspiffs -c spiffs_content -b 4096 -p 256 -s 0x170000 spiffs_image.img
-#esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 write_flash --flash_size detect 0x290000 spiffs_image.img
+#mkspiffs -c $SRCPATH -b 4096 -p 256 -s 0x170000 $IMAGEFILE
+#esptool.py --chip esp32 --port $2 --baud 921600 write_flash --flash_size detect 0x290000 $IMAGEFILE
 ## this line is used with the debugging partitions file
-mkspiffs -c spiffs_content -b 4096 -p 256 -s 0x1F0000 spiffs_image.img
-esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 write_flash --flash_size detect 0x210000 spiffs_image.img
+mkspiffs -c $SRCPATH -b 4096 -p 256 -s 0x1F0000 $IMAGEFILE
+esptool.py --chip esp32 --port $2 --baud 921600 write_flash --flash_size detect 0x210000 $IMAGEFILE
